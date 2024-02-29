6306e1c2fd560ecff0b6d593f7b7bc497a8fd98b
Alex Davies <traverse.da@gmail.com>
2024-02-29 07:42:05 -0400

    Auto apply oh-my-zsh

--- run_onchange_00_init.sh
+++ run_onchange_00_init.sh
@@ -2,27 +2,20 @@
 set -Eeuo pipefail
 
 case $(uname -m) in
-	"x86_64")
-		nix_portable=https://github.com/DavHau/nix-portable/releases/download/v010/nix-portable
-		;;
-	"aarch64")
-		nix_portable=https://github.com/DavHau/nix-portable/releases/download/v010/nix-portable-aarch64
+"x86_64")
+	nix_portable=https://github.com/DavHau/nix-portable/releases/download/v010/nix-portable
+	;;
+"aarch64")
+	nix_portable=https://github.com/DavHau/nix-portable/releases/download/v010/nix-portable-aarch64
+	;;
 esac
 
-if ! command -v nix-shell &> /dev/null
-#Check if nix already exists on this system, if it does we don't
-#want to install portable nix.
-then
+if ! command -v nix-shell &>/dev/null; then #Check if nix already exists on this system, if it does we don't
+	#want to install portable nix.
 	curl -L $nix_portable -o ~/.local/bin/nix-portable
 	chmod +x ~/.local/bin/nix-portable
 	ln -s ~/.local/bin/nix-portable ~/.local/bin/nix-shell
 	ln -s ~/.local/bin/nix-portable ~/.local/bin/nix
 fi
 
-# ~/.local/bin/nix run nixpkgs#pipx install pushbullet-cli
-
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-#Git repos with large files will break without this.
-git config --global --add oh-my-zsh.hide-dirty 1
-
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || true
