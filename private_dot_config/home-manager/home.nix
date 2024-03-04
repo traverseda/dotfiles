084bb6cda281f92484f2ef76cb54058e17a741a7
Alex Davies <traverse.da@gmail.com>
2024-03-04 07:22:21 -0400

    Work on integrating home-manager into dotfiles

---
+++ private_dot_config/home-manager/home.nix
@@ -0,0 +1,149 @@
+{ config, pkgs, lib, ... }:
+
+let
+  ros = import (
+    builtins.fetchTarball https://github.com/lopsided98/nix-ros-overlay/archive/master.tar.gz
+  ) {};
+  fromGitHub = ref: repo: pkgs.vimUtils.buildVimPlugin {
+    pname = "${lib.strings.sanitizeDerivationName repo}";
+    version = ref;
+    src = builtins.fetchGit {
+      url = "https://github.com/${repo}.git";
+      ref = ref;
+    };
+  };
+in
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "traverseda";
+  home.homeDirectory = "/home/traverseda";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    pkgs.htop
+    pkgs.zsh
+    pkgs.xclip
+    pkgs.mosh
+    pkgs.waypipe
+    pkgs.pwgen
+    pkgs.chezmoi
+    pkgs.git
+
+    pkgs.logseq
+    pkgs.wl-clipboard
+    pkgs.nextcloud-client
+    pkgs.vscodium
+    pkgs.inkscape
+    pkgs.gimp
+    pkgs.krita
+    pkgs.qgroundcontrol
+
+    pkgs.signal-desktop
+    pkgs.element-desktop
+
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+programs.zsh = {
+  enable = true;
+  enableCompletion = true;
+  enableAutosuggestions = true;
+  syntaxHighlighting.enable = true;
+
+  history.size = 10000;
+  history.path = "${config.xdg.dataHome}/zsh/history";
+  oh-my-zsh = {
+   enable = true;
+   plugins = [ "git" "docker" "docker-compose"];
+   theme = "robbyrussell";
+  }; 
+};
+
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    plugins = with pkgs.vimPlugins; [
+      nvim-lspconfig
+      nvim-treesitter.withAllGrammars
+      vim-bufferline
+      tokyonight-nvim
+      which-key-nvim
+    ];
+  };
+  programs.firefox = {
+        profiles = {
+        default = {
+
+      enable = true;
+	  extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+	    ublock-origin
+	    bitwarden
+	    darkreader
+    ];
+    };
+    };
+
+  };
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. If you don't want to manage your shell through Home
+  # Manager then you have to manually source 'hm-session-vars.sh' located at
+  # either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/traverseda/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  programs.home-manager.enable = true;
+}
