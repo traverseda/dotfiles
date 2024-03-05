43b4039ef6842cc5355cd75c50d58ca9d43a02f6
Alex Davies <traverse.da@gmail.com>
2024-03-05 11:58:38 -0400

    Work on migrating fully to nix

--- run_onchange_00_init.sh
+++ run_onchange_00_init.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
+#
+# Boostrap nix package manager and home-manager
+#
 set -e
 
 case $(uname -m) in
@@ -10,12 +14,28 @@ case $(uname -m) in
 	;;
 esac
 
+check_sudo() {
+	if [ "$(id -u)" != "0" ]; then
+		echo "This script prefers to use sudo to install nixos globally. (y/n)"
+		read -r response
+		if [ "$response" == "y" ] || [ "$response" == "Y" ]; then
+			echo "Please enter your password for sudo commands."
+
+			# Add your specific commands here with sudo
+			sudo sh <(curl -L https://nixos.org/nix/install) --daemon
+
+		else
+			echo "Installing using nix-portable instead."
+			curl -L $nix_portable -o ~/.local/bin/nix-portable
+			chmod +x ~/.local/bin/nix-portable
+			ln -s ~/.local/bin/nix-portable ~/.local/bin/nix-shell
+			ln -s ~/.local/bin/nix-portable ~/.local/bin/nix
+		fi
+	fi
+}
+
 if ! command -v nix-shell &>/dev/null; then #Check if nix already exists on this system, if it does we don't
-	#want to install portable nix.
-	curl -L $nix_portable -o ~/.local/bin/nix-portable
-	chmod +x ~/.local/bin/nix-portable
-	ln -s ~/.local/bin/nix-portable ~/.local/bin/nix-shell
-	ln -s ~/.local/bin/nix-portable ~/.local/bin/nix
+	check_sudo
 fi
 
 # Check if ~/.local/bin/ is in the PATH
@@ -28,4 +48,6 @@ fi
 
 source ~/.bashrc
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc || true
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+nix-shell '<home-manager>' -A install
